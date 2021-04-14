b740c55b52761eb0495e58b2ba4aed8caf4bbb3b
Gordienko, Mikhail (ext) (CT RDA BAM SMR-DE) <mikhail.gordienko.ext@siemens.com>
2021-04-14 09:22:22 +0300

    feature: 001 make initial commit

---
+++ src/test/java/com/example/playgitflow/PlayGitflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.playgitflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayGitflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
